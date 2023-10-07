5030a6ffc89ca444db81e14c120e2714fb2dddd9
Mary Zotova <zotovamaryan@gmail.com>
2023-10-07 16:10:43 +0300

    Added Task3 class.
    Added Task3Test class with unit tests.

---
+++ src/test/java/edu/hw1/test3/Task3Test.java
@@ -0,0 +1,93 @@
+package edu.hw1.test3;
+
+import edu.hw1.task3.Task3;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Task3Test {
+
+    @Test
+    void sortedArrays() {
+        Task3 task3 = new Task3();
+
+        int[] array1 = {1, 2, 3, 4};
+        int[] array2 = {0, 6};
+
+        boolean canNest = task3.isNestable(array1, array2);
+
+        assertThat(canNest).isTrue();
+    }
+
+    @Test
+    void notSortedArrays() {
+        Task3 task3 = new Task3();
+
+        int[] array1 = {3, 1};
+        int[] array2 = {4, 0};
+
+        boolean canNest = task3.isNestable(array1, array2);
+
+        assertThat(canNest).isTrue();
+    }
+
+    @Test
+    void equalValuesArrays() {
+        Task3 task3 = new Task3();
+
+        int[] array1 = {9, 9, 8};
+        int[] array2 = {8, 9};
+
+        boolean canNest = task3.isNestable(array1, array2);
+
+        assertThat(canNest).isFalse();
+    }
+
+    @Test
+    void notNestableArrays() {
+        Task3 task3 = new Task3();
+
+        int[] array1 = {1, 2, 3, 4};
+        int[] array2 = {2, 3};
+
+        boolean canNest = task3.isNestable(array1, array2);
+
+        assertThat(canNest).isFalse();
+    }
+
+    @Test
+    void emptyFirstArray() {
+        Task3 task3 = new Task3();
+
+        int[] array1 = {};
+        int[] array2 = {1, 3, 6, 7};
+
+        boolean canNest = task3.isNestable(array1, array2);
+
+        assertThat(canNest).isTrue();
+    }
+
+    @Test
+    void emptySecondArray() {
+        Task3 task3 = new Task3();
+
+        int[] array1 = {1, 2};
+        int[] array2 = {};
+
+        boolean canNest = task3.isNestable(array1, array2);
+
+        assertThat(canNest).isFalse();
+    }
+
+    @Test
+    void emptyBothArrays() {
+        Task3 task3 = new Task3();
+
+        int[] array1 = {};
+        int[] array2 = {};
+
+        boolean canNest = task3.isNestable(array1, array2);
+
+        assertThat(canNest).isFalse();
+    }
+
+}
