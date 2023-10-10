a344911d2935754e05140a4c5df8722da62fc76d
Mary Zotova <zotovamaryan@gmail.com>
2023-10-10 22:01:54 +0300

    Fixed constant variables' names using uppercase with underscore to follow naming convention in Task1 class.

--- src/main/java/edu/hw1/task1/Task1.java
+++ src/main/java/edu/hw1/task1/Task1.java
@@ -4,16 +4,18 @@ import java.util.regex.Pattern;
 
 public class Task1 {
 
-    final String pattern = "[0-9]{2,}:[0-5]{1}[0-9]{1}";
-    final int minuteInSeconds = 60;
+    // make a boundary for minutes value with 2 to 4 digits,
+    // as 9999 minutes ~ 7 days, there are no videos that last so long
+    private static final String PATTERN = "[0-9]{2,4}:[0-5]{1}[0-9]{1}";
+    private static final int MINUTE_IN_SECONDS = 60;
 
     public int timeToSeconds(String videoTime) {
 
-        if (Pattern.matches(pattern, videoTime)) {
+        if (Pattern.matches(PATTERN, videoTime)) {
 
             String[] numbersStr = videoTime.split(":");
 
-            return Integer.valueOf(numbersStr[0]) * minuteInSeconds + Integer.valueOf(numbersStr[1]);
+            return Integer.valueOf(numbersStr[0]) * MINUTE_IN_SECONDS + Integer.valueOf(numbersStr[1]);
 
         } else {
             return -1;
