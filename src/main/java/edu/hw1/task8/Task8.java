5c73b2801bf20454065feffc8e969166b908af89
Mary Zotova <zotovamaryan@gmail.com>
2023-10-10 23:15:34 +0300

    Fixed constant variables' names using uppercase with underscore to follow naming convention in Task8 class.

--- src/main/java/edu/hw1/task8/Task8.java
+++ src/main/java/edu/hw1/task8/Task8.java
@@ -2,23 +2,23 @@ package edu.hw1.task8;
 
 public class Task8 {
 
-    private final int chessBoardSize = 8;
+    private final static int CHESS_BOARD_SIZE = 8;
 
     public boolean isOnBoard(int x, int y) {
-        return (x >= 0 && x < chessBoardSize && y >= 0 && y < chessBoardSize);
+        return (x >= 0 && x < CHESS_BOARD_SIZE && y >= 0 && y < CHESS_BOARD_SIZE);
     }
 
-    private final int[][] moves = {{-1, -2}, {-2, -1}, {1, -2}, {2, -1}};
+    private final static int[][] MOVES = {{-1, -2}, {-2, -1}, {1, -2}, {2, -1}};
 
     public boolean areKnightsSafe(int[][] array) {
 
-        for (int i = 0; i < chessBoardSize; i++) {
-            for (int j = 0; j < chessBoardSize; j++) {
+        for (int i = 0; i < CHESS_BOARD_SIZE; i++) {
+            for (int j = 0; j < CHESS_BOARD_SIZE; j++) {
                 if (array[i][j] == 1) {
 
-                    for (int m = 0; m < moves.length; m++) {
-                        int x = i + moves[m][0];
-                        int y = j + moves[m][1];
+                    for (int m = 0; m < MOVES.length; m++) {
+                        int x = i + MOVES[m][0];
+                        int y = j + MOVES[m][1];
 
                         if (isOnBoard(x, y) && array[x][y] == 1) {
                             return false;
