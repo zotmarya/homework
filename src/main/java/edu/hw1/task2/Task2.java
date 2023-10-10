0556c6b254afb7ee7420036c62fce98b418a30cc
Mary Zotova <zotovamaryan@gmail.com>
2023-10-10 22:08:21 +0300

    Changed usage of ternary operator to Math.abs() method in Task2 class.

--- src/main/java/edu/hw1/task2/Task2.java
+++ src/main/java/edu/hw1/task2/Task2.java
@@ -4,7 +4,7 @@ public class Task2 {
 
     int digitsCount(int number) {
 
-        int absNumber = number < 0 ? -number : number;
+        int absNumber = Math.abs(number);
 
         return (absNumber == 0) ? 1 : (int) (Math.log10(absNumber) + 1);
     }
