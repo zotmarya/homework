3d5ca7a35cedbed12c8e7b33149897d38cdd60da
Mary Zotova <zotovamaryan@gmail.com>
2023-10-20 21:33:20 +0300

    Added Connection and ConnectionManager interfaces; ConnectionException,  FaultyConnection, FaultyConnectionManager, PopularCommandExecutor, RandomNumGenerator, StableConnection classes.

---
+++ src/main/java/edu/hw2/task3/ConnectionException.java
@@ -0,0 +1,12 @@
+package edu.hw2.task3;
+
+public class ConnectionException extends RuntimeException {
+
+    public ConnectionException(String message) {
+        super(message);
+    }
+
+    public ConnectionException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
