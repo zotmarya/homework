f32f079e7521802680edff78d657c604c7bb8188
Mary Zotova <zotovamaryan@gmail.com>
2023-10-21 12:59:14 +0300

    Edited WordHandler, HangmanGame and HangmanPictureHandler classes.

---
+++ src/main/java/edu/project1/HangmanPictureHandler.java
@@ -0,0 +1,62 @@
+package edu.project1;
+
+import java.io.FileReader;
+import java.io.IOException;
+
+public class HangmanPictureHandler {
+
+    private static HangmanPictureHandler hangmanPictureHandler;
+    private static final int hangmanPicsAmount = 6;
+    private static final String[] hangmanPictures;
+
+    static {
+        hangmanPictures = new String[hangmanPicsAmount];
+        readTxtFileWithHangmanPictures();
+    }
+
+    private static void readTxtFileWithHangmanPictures() {
+        StringBuilder picture = new StringBuilder();
+
+        try (FileReader fileReader = new FileReader("src/main/resources/project1/hangman-pictures.txt")) {
+            char symbol;
+            int symbolCode;
+            int index = 0;
+
+            while (index < hangmanPicsAmount && (symbolCode = fileReader.read()) != -1) {
+                symbol = (char) symbolCode;
+                if (symbol == '$') {
+                    hangmanPictures[index++] = picture.toString();
+                    picture.setLength(0);
+                } else {
+                    picture.append(symbol);
+                }
+            }
+
+        } catch (IOException exception) {
+        }
+    }
+
+    public String getHangmanPicture(int amountOfMadeMistakes) {
+        return switch (amountOfMadeMistakes) {
+            case 0 -> hangmanPictures[0];
+            case 1 -> hangmanPictures[1];
+            case 2 -> hangmanPictures[2];
+            case 3 -> hangmanPictures[3];
+            case 4 -> hangmanPictures[4];
+            case 5 -> hangmanPictures[5];
+
+            default -> "Undefined argument.";
+        };
+    }
+
+    public static HangmanPictureHandler getInstance () {
+        if (hangmanPictureHandler == null) {
+            hangmanPictureHandler = new HangmanPictureHandler();
+        }
+
+        return hangmanPictureHandler;
+    }
+
+    private HangmanPictureHandler() {
+    }
+}
--- src/main/java/edu/project1/WordHandler.java
+++ src/main/java/edu/project1/WordHandler.java
@@ -1,48 +1,67 @@
 package edu.project1;
 
 public class WordHandler {
+
+    private static WordHandler wordHandler;
     private char[] wordSymbols;
     private char[] guessedLetters;
+    private int wordLength;
 
-    private int wordLen;
-
-    public WordHandler(String word) {
+    public void setNewWord(String word) {
         this.wordSymbols = word.toCharArray();
         guessedLetters = new char[wordSymbols.length];
 
-        wordLen = wordSymbols.length;
+        wordLength = wordSymbols.length;
 
+        fillArrayWithAsterisks();
+    }
+
+    public static WordHandler getInstance() {
+        if (wordHandler == null) {
+            wordHandler = new WordHandler();
+        }
+
+        return wordHandler;
+    }
+
+    private WordHandler() {
+    }
+
+    private void fillArrayWithAsterisks() {
         for (int i = 0; i < guessedLetters.length; i++) {
             guessedLetters[i] = '*';
         }
     }
 
-    public int getWordLen() {
-        return wordLen;
-    }
-
-    public String checkIfGuessedLetter (char letter) {
-
+    public int checkIfPlayerGuessedLetter(char letter) {
+        int guessedAmount = 0;
         boolean hasGuessed = false;
 
         for (int i = 0; i < wordSymbols.length; i++) {
-            if(letter == wordSymbols[i]) {
-                if(guessedLetters[i] == letter) return "YOU'VE ALREADY GUESSED THIS LETTER 🤔";
+            if (letter == wordSymbols[i]) {
+                if (guessedLetters[i] == letter) {
+                    return 0;
+                }
 
                 guessedLetters[i] = wordSymbols[i];
+                guessedAmount++;
 
                 hasGuessed = true;
             }
         }
 
-        return hasGuessed == true ? "CORRECT 😎" : "NO LUCK 😔";
+        return hasGuessed == true ? guessedAmount : -1;
     }
 
-    public char[] getGuessedLetters() {
-        return guessedLetters;
+    public int getWordLength() {
+        return wordLength;
     }
 
     public String getGuessedLettersString() {
         return new String(guessedLetters);
+    }
+
+    public String getWord() {
+        return new String(wordSymbols);
     }
 }
