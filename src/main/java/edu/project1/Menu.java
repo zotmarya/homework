57ebd19b32d4b27cff0886d68ebbee1388435866
Mary Zotova <zotovamaryan@gmail.com>
2023-10-21 12:58:12 +0300

    Added methods to Menu class.

--- src/main/java/edu/project1/Menu.java
+++ src/main/java/edu/project1/Menu.java
@@ -1,5 +1,31 @@
 package edu.project1;
 
 public class Menu {
+    private static Menu menu;
 
+    public static Menu getInstance() {
+        if (menu == null) {
+            menu = new Menu();
+        }
+
+        return menu;
+    }
+
+    public String outputMenu() {
+        return """
+
+
+                        HANGMAN (ENGLISH VERSION)
+                           1 - PLAY HANGMAN
+                           2 - EXIT
+
+            """;
+    }
+
+    public String askForPlayerChoice() {
+        return "YOUR CHOICE: ";
+    }
+
+    private Menu() {
+    }
 }
