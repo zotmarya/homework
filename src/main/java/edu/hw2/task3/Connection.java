dd71205b3be0e26e56c6e94222aa873beb3b7aa7
Mary Zotova <zotovamaryan@gmail.com>
2023-11-07 16:50:35 +0300

    Removed files.

--- src/main/java/edu/hw2/task3/Connection.java
+++
@@ -1,5 +0,0 @@
-package edu.hw2.task3;
-
-public interface Connection extends AutoCloseable {
-    void execute(String command);
-}
--- src/main/java/edu/hw2/task3/FaultyConnectionManager.java
+++
@@ -1,8 +0,0 @@
-package edu.hw2.task3;
-
-public class FaultyConnectionManager implements ConnectionManager {
-    @Override
-    public Connection getConnection() {
-        return new FaultyConnection();
-    }
-}
