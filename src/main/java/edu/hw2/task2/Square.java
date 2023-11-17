bb255d1cd3194758490966a4448c92af03dd57e6
Mary Zotova <zotovamaryan@gmail.com>
2023-11-17 12:33:20 +0300

    Revert "Removed files."
    This reverts commit dd71205b3be0e26e56c6e94222aa873beb3b7aa7.

---
+++ src/main/java/edu/hw2/task2/Square.java
@@ -0,0 +1,11 @@
+package edu.hw2.task2;
+
+public class Square extends Rectangle {
+    public void setSize(int size) {
+        height = size;
+        width = size;
+    }
+
+    public Square() {
+    }
+}
